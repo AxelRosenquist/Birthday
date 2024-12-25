b6f2232b5b6bbfc3689b25f678ff325c95e5daf7
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2024-12-25 19:02:30 +0100

    Started work on function for guessing characters

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let string = 'Den tjugoandra mars ska du och jag se Molly Hammar i Malmö. I presenten ingår även restaurangbesök och annat du vill hitta på!';
 let cryptString = '';
-let dictionary = {1: 'g', 2: 'a', 3: 'ö', 4: 'k', 5: 'b', 6: 'd', 7: 'o', 8: 't', 9: 'p', 10: 'v', 11: 'n', 12: 'e', 13: 'ä', 14: 'i', 15: 'å', 16: 's', 17: 'l', 18: 'c', 19: 'h', 20: 'j', 21: 'm', 22: 'u', 23: 'r', 24: 'y'};
+let dictionary = {1: 'g', 2: 'a', 3: 'ö', 4: 'k', 5: 'b', 6: 'd', 7: 'o', 8: 't', 9: 'p', 10: 'v', 11: 'n', 12: 'e', 13: 'ä', 14: 'i', 15: 'å', 16: 's', 17: 'l', 18: 'c', 19: 'h', 20: 'j', 21: 'm', 22: 'u', 23: 'r', 24: 'y', 25: ' '};
 
 string.toLowerCase().split('').forEach((letter) => {
-    if (!(letter == ' ' || letter == '.' || letter == '!' || letter == ',')){
+    if (!(letter == '.' || letter == '!' || letter == ',')){
         for (let key in dictionary){
             if (dictionary[key] == letter){
                 cryptString += (key.toString() + ' ' ) 
@@ -14,4 +14,14 @@ string.toLowerCase().split('').forEach((letter) => {
     }
 });
 
-document.getElementById("secretString").innerHTML = cryptString;
+document.getElementById('secretString').innerHTML = cryptString;
+
+let number;
+let character;
+document.getElementById('submit').onclick = function(){
+    event.preventDefault();
+    number = document.getElementById('number').value;
+    character = document.getElementById('character').value;
+    console.log(number + ' AND ' + character);
+};
+
