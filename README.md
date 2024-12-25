424cb0eefaad7f0aa5bcf2463f18554a0a17349d
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2024-12-25 16:30:12 +0100

    Made dict with javascript of the unique letters of the sentance

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Birthday
-
-Den 22e mars ska du och jag se Molly Hammar i Malmö. I presenten ingår även restaurangbesök och annat du vill hitta på!
+Den tjugoandra mars ska du och jag se Molly Hammar i Malmö. I presenten ingår även restaurangbesök och annat du vill hitta på!
---
+++ script.js
@@ -0,0 +1,5 @@
+let string = 'den tjugoandra mars ska du och jag se Molly Hammar i Malmö. I presenten ingår även restaurangbesök och annat du vill hitta på!';
+let cryptString;
+let dictionary = {1: 'g', 2: 'a', 3: 'ö', 4: 'k', 5: 'b', 6: 'd', 7: 'o', 8: 't', 9: 'p', 10: 'v', 11: 'n', 12: 'e', 13: 'ä', 14: 'i', 15: 'å', 16: 's', 17: 'l', 18: 'c', 19: 'h', 20: 'j', 21: 'm', 22: 'u', 23: 'r', 24: 'y'};
+console.log(dictionary);
+
