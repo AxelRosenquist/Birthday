d2cc28cce8f01980e69a271c26aa501a6f354a44
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2024-12-25 23:57:16 +0100

    App in finished working state

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ string.toLowerCase().split('').forEach((letter) => {
         cryptString += letter 
     }
 });
+let currentString = cryptString;
 
 document.getElementById('secretString').innerHTML = cryptString;
 
@@ -20,8 +21,18 @@ let number;
 let character;
 document.getElementById('submit').onclick = function(){
     event.preventDefault();
-    number = document.getElementById('number').value;
+    number = Number(document.getElementById('number').value);
     character = document.getElementById('character').value;
-    console.log(number + ' AND ' + character);
+    console.log('_' + String(number) + ' ' + '_')
+    
+    if (dictionary[number] == character){
+        currentString = currentString.replaceAll(String(number) + ' ',  character)
+        console.log('You guessed Right');
+        console.log(currentString);
+        document.getElementById('secretString').innerHTML = currentString;
+    }else{
+        console.log('You guessed wrong');
+
+    }
 };
 
