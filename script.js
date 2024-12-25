ea4822a085b9530f6b1e7b65759af9fcbde22c76
Axel Rosenquist <Axel.Rosenquist4@gmail.com>
2024-12-25 16:55:21 +0100

    got the js to html working, also encoded the string with the dict

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let string = 'den tjugoandra mars ska du och jag se Molly Hammar i Malmö. I presenten ingår även restaurangbesök och annat du vill hitta på!';
-let cryptString;
+let string = 'Den tjugoandra mars ska du och jag se Molly Hammar i Malmö. I presenten ingår även restaurangbesök och annat du vill hitta på!';
+let cryptString = '';
 let dictionary = {1: 'g', 2: 'a', 3: 'ö', 4: 'k', 5: 'b', 6: 'd', 7: 'o', 8: 't', 9: 'p', 10: 'v', 11: 'n', 12: 'e', 13: 'ä', 14: 'i', 15: 'å', 16: 's', 17: 'l', 18: 'c', 19: 'h', 20: 'j', 21: 'm', 22: 'u', 23: 'r', 24: 'y'};
-console.log(dictionary);
 
+string.toLowerCase().split('').forEach((letter) => {
+    if (!(letter == ' ' || letter == '.' || letter == '!' || letter == ',')){
+        for (let key in dictionary){
+            if (dictionary[key] == letter){
+                cryptString += (key.toString() + ' ' ) 
+            }
+        }
+    } else {
+        cryptString += letter 
+    }
+});
+
+document.getElementById("secretString").innerHTML = cryptString;
